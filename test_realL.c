c210ec57edeb507e00cb49e07847bb2221dd5892
Andy Rodríguez <androdl96@gmail.com>
2024-09-11 15:38:15 +0200

    Create c file for real line example

---
+++ test_realL.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <math.h>
+#include <time.h>
+
+#define MAX_LINES 908 // Hacer esto no manual, maybe con wc
+
+int main() {
+
+    
+    printf("Debug | Exit\n");
+    return 0;
+}
